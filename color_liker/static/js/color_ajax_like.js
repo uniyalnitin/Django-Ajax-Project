c289c6a4d7b6c322e9d5be7e34276c572153b408
= <nitinuniyal21@gmail.com>
2018-09-22 16:06:50 +0530

    static files loading is fixed now

--- color_liker/static/js/color_ajax_like.js
+++ color_liker/static/js/color_ajax_like.js
@@ -3,14 +3,15 @@ var MILLS_TOIGNORE_LIKES = 500;
 var processLike = function(){
 	var $button_just_clicked_on = $(this);
 
-	var color_url = $button_just_clicked_on.data('color_url');
-
+	var color_id = $button_just_clicked_on.data('color_id');
+	
  	var processServerResponse = function(serverResponse_data, textStatus_ignored,jqXHR_ignored)  {
- 		$('#toggle_color_like_cell' + color_id).html(serverResponse_data);
+
+ 		$('#toggle__color_like_cell_' + color_id).html(serverResponse_data);
  	}
 
  	var config = {
-	 	url :color_url,
+	 	url :'/color_liker/like_color_/'+color_id,
  		dataType : 'html',
  		success: processServerResponse
 	};
@@ -18,6 +19,6 @@ var processLike = function(){
 };
 
 $(document).ready(function(){
-	$('.td__toggle_color_like_button').click(_debounce(processLike,MILLS_TOIGNORE_LIKES,true));
+	$('.td__toggle_color_like_button').click(processLike);
 
 });
--- color_liker/urls.py
+++ color_liker/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
 from color_liker import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path("", views.ColorList.as_view(),name= "color_list"),
 	path('like_color_/<int:color_id>/', views.toggle_color_like,name="toggle_color_like"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
